cb1485e119b252f4d8920de50f0b020b7809ce47
Leon2213 <48647601+Leon2213@users.noreply.github.com>
2023-09-04 15:11:09 +0200

    Update Database.java

--- src/sample/Database.java
+++ src/sample/Database.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 
 
-
+// MOCK DATABASE CLASS
 public class Database {
     private final List<Case> allCases = new ArrayList<>();
 
@@ -20,18 +20,18 @@ public class Database {
     }
 
     private void loadData() {
-        Case case1 = new Case("Blixten har slagit ner i datorn", Category.ORDER, "2021-06-30");
-        case1.addComment("Användaren säger att hon har haft fel på sin dator länge", "user X");
-        case1.addComment("Användaren ringer in igen och undrar hur det går", "user X");
-        case1.addComment("Tekniker har åtgärdat felet. Bekräftas av användaren", "user X");
+        Case case1 = new Case("Lightning hit my PC", Category.ORDER, "2021-06-30");
+        case1.addComment("User says her PC been broken for some time", "user X");
+        case1.addComment("User is calling back requesting status", "user X");
+        case1.addComment("The case has been resolved by technician", "user X");
         case1.closeCase();
         addCase(case1);
 
 
-        addCase(new Case("Fel på min dator", Category.HARDWARE_ERROR, "2021-07-01"));
-        addCase(new Case("Jag behöver hjälp med mitt system", Category.APPLICATION_SUPPORT, "2021-07-05"));
-        addCase(new Case("Mitt system fungerar inte", Category.APPLICATION_ERROR, "2021-06-25"));
-        addCase(new Case("Vill beställa ett nytt tangentbord", Category.ORDER, "2021-06-30"));
+        addCase(new Case("My PC is broken!", Category.HARDWARE_ERROR, "2021-07-01"));
+        addCase(new Case("I need help with the new IT system", Category.APPLICATION_SUPPORT, "2021-07-05"));
+        addCase(new Case("My order system doesn't work!", Category.APPLICATION_ERROR, "2021-06-25"));
+        addCase(new Case("want to order a new keyboard", Category.ORDER, "2021-06-30"));
 
 
     }
